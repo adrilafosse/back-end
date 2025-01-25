997275baaf023eba3705a1243b8efa085badb7c1
adrilafosse <81637698+adrilafosse@users.noreply.github.com>
2025-01-25 15:48:36 +0100

    ok

--- back-end.py
+++ back-end.py
@@ -10,47 +10,60 @@ api_key = os.getenv('API_KEY')  # Accès à la variable d'environnement
 
 url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent"
 
-@app.route('/Exemple', methods=['GET'])
+codes = []
+
+@app.route('/Cookie', methods=['GET'])
+def Cookie():
+    code = os.urandom(16).hex()
+    codes.append(code)
+
+    return code
+
+@app.route('/Exemple', methods=['POST'])
 def Exemple():
-    texte = """Génère moi un JSON contenant 1 nouveau exemple de prompt en t'inspirant de ces 9 exemples :}" :
-    Les présidents de la 5e république en France,
-    Les présidents des Etats-Unis,
-    Les rois de France,
-    Jérôme à 3 frères et 4 soeurs,
-    Ludivine a visité ces pays hors de l'Europe : Vietnam, Etats-Unis, Canada, Martinique et Tunisie,
-    Adrien est allé faire du ski à Risoul, Val d'isère, Isola 2000, Alpes d'Huez, les Arcs, Courchevelle, les Menuires, le Sauze et Avoriaz et j'aimerais retourner à l'Alpes d'Huez,
-    Le plat traditionnel Canadien est la poutine,
-    Le Beausset compte 1098 habitants en 2022 et elle est catégorisée petite ville,
-    Les monuments emblématiques de Paris,
-    Les fleuves principaux en Europe,
-    Camille a déjà visité des îles tropicales,
-    Le plat national espagnol est la paella, souvent préparée avec du riz, des fruits de mer, du poulet et des épices,
-    Martin collectionne les timbres représentant des animaux en voie de disparition,
-    Une des capitales des pays nordiques est Copenhague,
-    Sophie adore les films de science-fiction comme Star Wars,
-    Les fromages français célèbres,
-    Jean a remporté la médaille d'or en athlétisme dans ces compétitions : les Jeux olympiques de 2012, le championnat d'Europe 2014 et les Mondiaux de 2015,
-    Le Mont-Blanc, situé dans les Alpes, est la plus haute montagne d'Europe occidentale avec une altitude de 4 807 mètres"""
+    data = request.get_json()
+    codeUtilisateur = data.get("code", "")
+    if codeUtilisateur in codes:
+        texte = """Génère moi un JSON contenant 1 nouveau exemple de prompt en t'inspirant de ces 9 exemples :
+            Les présidents de la 5e république en France,
+            Les présidents des Etats-Unis,
+            Les rois de France,
+            Jérôme à 3 frères et 4 soeurs,
+            Ludivine a visité ces pays hors de l'Europe : Vietnam, Etats-Unis, Canada, Martinique et Tunisie,
+            Adrien est allé faire du ski à Risoul, Val d'isère, Isola 2000, Alpes d'Huez, les Arcs, Courchevelle, les Menuires, le Sauze et Avoriaz et j'aimerais retourner à l'Alpes d'Huez,
+            Le plat traditionnel Canadien est la poutine,
+            Le Beausset compte 1098 habitants en 2022 et elle est catégorisée petite ville,
+            Les monuments emblématiques de Paris,
+            Les fleuves principaux en Europe,
+            Camille a déjà visité des îles tropicales,
+            Le plat national espagnol est la paella, souvent préparée avec du riz, des fruits de mer, du poulet et des épices,
+            Martin collectionne les timbres représentant des animaux en voie de disparition,
+            Une des capitales des pays nordiques est Copenhague,
+            Sophie adore les films de science-fiction comme Star Wars,
+            Les fromages français célèbres,
+            Jean a remporté la médaille d'or en athlétisme dans ces compétitions : les Jeux olympiques de 2012, le championnat d'Europe 2014 et les Mondiaux de 2015,
+            Le Mont-Blanc, situé dans les Alpes, est la plus haute montagne d'Europe occidentale avec une altitude de 4 807 mètres"""
+        
 
-    response = requests.post(f"{url}?key={api_key}", 
-        json= {
-            "contents": [
-                {
-                    "parts": [
-                        {
-                            "text": texte
-                        },
-                    ],
-                },
-            ],
-        }, 
-        headers={
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        })
+        reponse = requests.post(f"{url}?key={api_key}", 
+            json= {
+                "contents": [
+                    {
+                        "parts": [
+                            {
+                                "text": texte
+                            },
+                        ],
+                    },
+                ],
+            }, 
+            headers={
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            })
 
-    if response.status_code == 200:
-        return jsonify(response.json())
+        if reponse.status_code == 200:
+            return jsonify(reponse.json())
     else:
         return jsonify({"error"})
 
@@ -58,34 +71,41 @@ def Exemple():
 def Generer():
     data = request.get_json()
     texte = data.get("texte", "")
-    
-    prompt_texte = f"""Génère moi un JSON avec une question, 1 bonne réponse et 3 mauvaises réponses à partir du texte suivant : "{texte}"
+    codeUtilisateur = data.get("code", "")
+    if codeUtilisateur in codes:
+        prompt_texte = f"""Génère moi un JSON avec une question, 1 bonne réponse et 3 mauvaises réponses à partir du texte suivant : "{texte}"
                        sous le forme {{question: 'question'}}, {{bonneReponse: 'bonne réponse'}}, {{mauvaiseReponse1: 'mauvaise réponse 1'}},
                        {{mauvaiseReponse2: 'mauvaise réponse 2'}}, {{mauvaiseReponse3: 'mauvaise réponse 3'}}"""
 
-    payload = {
-        "contents": [
-            {
-                "parts": [
-                    {
-                        "text": prompt_texte
-                    },
-                ],
-            },
-        ],
-    }
+        payload = {
+            "contents": [
+                {
+                    "parts": [
+                        {
+                            "text": prompt_texte
+                        },
+                    ],
+                },
+            ],
+        }
 
-    headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    }
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
 
-    response = requests.post(f"{url}?key={api_key}", json=payload, headers=headers)
+        reponse = requests.post(f"{url}?key={api_key}", json=payload, headers=headers)
 
-    if response.status_code == 200:
-        return jsonify(response.json())
+        if reponse.status_code == 200:
+            return jsonify(reponse.json())
     else:
         return jsonify({"error"})
     
+@app.route('/Supprimer', methods=['POST'])
+def Supprimer():
+    data = request.get_json()
+    codeUtilisateur = data.get("code", "")
+    if codeUtilisateur in codes:
+        codes.remove(codeUtilisateur)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
